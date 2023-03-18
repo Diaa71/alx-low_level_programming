a6cf890839e1bf38f28beb9c8a8201e30b31feb1
diaa71 <di.ayman@nu.edu.eg>
2023-03-17 19:16:40 -0700

    Task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+/*
+ * File: 100-print_comb3.c
+ * Diaa Azzam
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Displays all the pairs of distinct digits in
+ * ascending order, separated by a comma and a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
