729e97eae0aec4bd60f0934f27885a71aed00f34
diaa71 <di.ayman@nu.edu.eg>
2023-04-21 18:28:41 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -8,16 +8,18 @@ void printinteger(va_list charlist);
 void printfloat(va_list charlist);
 void printstring(va_list charlist);
 
+/* struct_characters - pointer to arguement */
 typedef struct characters
 {
-        /* pointer to the arguments passed to the function print_all) */
-        char *arguments;
-        /* pointer to function print_all that returns nothing */
-        /* and receive a list of characters(arguments) */
+	/* struct_characters - pointer to arguement */
+	/* pointer to the arguments passed to the function print_all) */
+	char *arguments;
+	/* pointer to function print_all that returns nothing */
+	/* and receive a list of characters(arguments) */
 	/* va_list is a type that iterates arguments */
 	/* va_list as type of  variable we're going to name charlist */
 	/* so the pointer points a function that receive va_list) */
-        void (*ptrfunc)(va_list);
+	void (*ptrfunc)(va_list);
 } characters;
 
 
