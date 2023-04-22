dad8b38803596b67d0e31364484e23ad65ac0035
diaa71 <di.ayman@nu.edu.eg>
2023-04-21 18:31:38 -0700

    task 0

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,16 +3,10 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-void printchar(va_list charlist);
-void printinteger(va_list charlist);
-void printfloat(va_list charlist);
-void printstring(va_list charlist);
-
-
-
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 
+#endif
